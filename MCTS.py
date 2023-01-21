bb08ea9491e5f723cef17e365646cacb7078aa32
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-01-21 14:50:19 +0000

    x

--- MCTS.py
+++ MCTS.py
@@ -6,23 +6,30 @@ from collections import Counter, defaultdict
 
 
 
+
 with open('data/cir_graph.p', 'rb') as f:
-    graph = pickle.load(f)
+    all_words_graph = pickle.load(f)
+
+def copy(object):
+    return {k: v.copy() for (k, v) in object.items()}
+
+def filter(object): # object = Counter
+    for val, key in object.items():
+        if key == 0:
+            object.pop(val)
 
 
 class Node:
-    char_graph = graph
-
-    def __init__(self, parent, curr_char, history = {}):
+    def __init__(self, parent, curr_char, graph = all_words_graph):
         self.n, self.w = 0, 0
         self.parent = parent
         self.curr_char = curr_char
-        self.history = history
+        self.graph = graph
         self.children = {}
 
     def select(self):
         children = self.children.values()
-        selectedChild = max(children, lambda x : x.UTC())
+        selectedChild = max(children, key = lambda x : x.UTC())
         return selectedChild
     
     def UTC(self):
@@ -32,19 +39,68 @@ class Node:
         return self.parent.n
     
     def expand(self):
+        
         for char in self.nextChar():
             if char not in self.children:
-                history = self.history.copy()
-                if char not in history:
-                    history[char] = set()
-                history[char].add(char)
-                return Node(self, char, )
+                graph_copy = copy(self.graph)
+                for c in changable.changable(self.curr_char):
+                    if c not in graph_copy:
+                        continue
+                    if char in graph_copy[c]:
+                        graph_copy[c][char] -= 1
+                        if graph_copy[c][char] == 0:
+                            graph_copy[c].pop(char)
+                child = Node(self, char, graph_copy)
+                self.children[char] = child
+                return child
 
     def nextChar(self):
-        result = []
+        result = Counter({})
         for char in changable.changable(self.curr_char):
-            if char in self.history:
-                result += list(Node.char_graph[char] - self.history[char])
-        return Counter(result)
+            if char not in self.graph:
+                continue
+            result += self.graph[char]
+        return result
         
+    def isEnd(self):
+        return False if self.nextChar() else True
+    
+    def isComplete(self):
+        return len(self.children) == len(self.nextChar())
+
+    def __str__(self):
+        return self.curr_char
+
+    def __repr__(self):
+        return f'({self.curr_char})'
+
+
+stack = []
+def simulate(root, stack):
+    ptr = root
+    stack.append(ptr)
+    while ptr.nextChar():
+        if ptr.isComplete():
+            ptr = ptr.select()
+        else:
+            ptr = ptr.expand()
+        stack.append(ptr)
+def backpropagate(stack):
+    alternater = True
+    while stack:
+        node = stack.pop()
+        node.n += 1
+        if alternater:
+            node.w += 1
+
+        alternater = not alternater
+
+def learn(root, stack,num = 100):
+    for i in range(num):
+        print(f'{i}회')
+        simulate(root, stack)
+        backpropagate(stack)
+    print(max(root.children, key = lambda x : root.children[x].w))
+
+learn(Node(None, '족'), stack, 1000)
 
---
+++ debug.py
@@ -0,0 +1,9 @@
+from collections import Counter
+
+c1 = Counter({'a' : 1, 'b' : 2})
+
+c2 = Counter({'a' : 2, 'c' : 1})
+
+
+
+print(not False)
